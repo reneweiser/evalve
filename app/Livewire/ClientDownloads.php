a7e943b97ec6ca9f2c018aa4dcbcbabfd1b4ed89
René Weiser <hello@reneweiser.de>
2025-09-08 14:54:25 +0200

    Added an update client action to dashboard widget

--- app/Livewire/ClientDownloads.php
+++ app/Livewire/ClientDownloads.php
@@ -2,16 +2,44 @@
 
 namespace App\Livewire;
 
+use Filament\Actions\Action;
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Components\FileUpload;
+use Filament\Notifications\Notification;
+use Filament\Schemas\Concerns\InteractsWithSchemas;
+use Filament\Schemas\Contracts\HasSchemas;
 use Filament\Widgets\Widget;
 use Illuminate\Support\Facades\Storage;
 
-class ClientDownloads extends Widget
+class ClientDownloads extends Widget implements HasActions, HasSchemas
 {
+    use InteractsWithActions;
+    use InteractsWithSchemas;
+
     protected string $view = 'livewire.client-downloads';
     public string $link = "";
 
     public function mount(): void
     {
         $this->link = Storage::url('public/clients/latest.zip');
+    }
+
+    public function updateClientAction(): Action
+    {
+        return Action::make('updateClient')
+            ->color('primary')
+            ->schema([
+                FileUpload::make('file')
+                    ->disk('public')
+                    ->directory('clients')
+                    ->getUploadedFileNameForStorageUsing(fn ($file) => 'latest.zip')
+            ])
+            ->action(function ($data) {
+                Notification::make()
+                    ->title('Client app updated')
+                    ->success()
+                    ->send();
+            });
     }
 }
--- resources/views/livewire/client-downloads.blade.php
+++ resources/views/livewire/client-downloads.blade.php
@@ -2,4 +2,11 @@
     <x-filament::section>
         <x-filament::link :href="$link">Download Client App</x-filament::link>
     </x-filament::section>
+    <x-filament::card>
+        <div>
+            {{ $this->updateClientAction }}
+
+            <x-filament-actions::modals />
+        </div>
+    </x-filament::card>
 </x-filament-widgets::widget>
