771adc0ea6a6ae64d0aa78f341bb51b77a77b684
René Weiser <hello@reneweiser.de>
2025-11-03 14:58:44 +0100

    Enhance polling view with full HTML structure and styling

--- resources/views/polling.blade.php
+++ resources/views/polling.blade.php
@@ -1,1 +1,16 @@
-<img src="{{ $image }}" alt="Polling Image">
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Polling</title>
+</head>
+<body>
+    <div class="bg-black w-full h-screen">
+        <img src="{{ $image }}" alt="Polling Image" class="w-full h-full object-cover">
+    </div>
+</body>
+</html>
